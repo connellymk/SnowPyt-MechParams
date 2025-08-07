4d1f87b424b5505701e95b2ec49743f5e69181c5
Mary Connelly <connellymarykate@gmail.com>
2025-08-07 16:10:34 -0400

    add descriptions

--- src/snowpyt_mechparams/elastic_modulus.py
+++ src/snowpyt_mechparams/elastic_modulus.py
@@ -1,1 +1,8 @@
 # Methods to calculate elastic modulus of a slab layer
+
+#Gerling et al. (2017) and improved in Bergfeld et al. (2023)
+
+#Other texts to consider are:
+# Srivastava et al. (2016)
+# Köchle and Schneebeli (2014)
+# Wautier et al. (2015)
--- src/snowpyt_mechparams/poissons_ratio.py
+++ src/snowpyt_mechparams/poissons_ratio.py
@@ -1,1 +1,4 @@
 # Methods to calculate Poisson's ratio (ν) of a slab layer
+
+# Sigrist (2006) and Mellor (1974)
+# Kochle and Schneebeli (2014)
